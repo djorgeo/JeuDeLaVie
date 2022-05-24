a70d61222abbd0c939644748a4c8953bcb9c0600
varzil94 <97194484+varzil94@users.noreply.github.com>
2022-05-24 11:03:30 +0200

    Add files via upload

--- grilleFrame.py
+++ grilleFrame.py
@@ -80,249 +80,6 @@ class GrilleFrame(tk.Frame, Grille):
     
     
     
-    '''
-    def clignotant(self):
-        """initialise la grille pour simuler le clignotant"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i][j - 1].naitre()
-        self.matrix[i][j - 1].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("10")
-
-    def ruche1(self):
-        """initialise la grille pour simuler la ruche"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i][j + 2].naitre()
-        self.matrix[i][j + 2].basculer()
-
-        self.matrix[i][j - 1].naitre()
-        self.matrix[i][j - 1].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("4")
-
-    def ruche2(self):
-        """initialise la grille pour simuler la génération de 4 ruches"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i + 1][j + 1].naitre()
-        self.matrix[i + 1][j + 1].basculer()
-
-        self.matrix[i + 2][j + 1].naitre()
-        self.matrix[i + 2][j + 1].basculer()
-
-        self.matrix[i + 1][j - 1].naitre()
-        self.matrix[i + 1][j - 1].basculer()
-
-        self.matrix[i + 2][j - 1].naitre()
-        self.matrix[i + 2][j - 1].basculer()
-
-        self.matrix[i + 3][j].naitre()
-        self.matrix[i + 3][j].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("17")
-
-    def balise(self):
-        """initialise la grille pour simuler la balise"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i + 1][j + 1].naitre()
-        self.matrix[i + 1][j + 1].basculer()
-
-        self.matrix[i + 2][j - 2].naitre()
-        self.matrix[i + 2][j - 2].basculer()
-
-        self.matrix[i + 3][j - 2].naitre()
-        self.matrix[i + 3][j - 2].basculer()
-
-        self.matrix[i + 3][j - 1].naitre()
-        self.matrix[i + 3][j - 1].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("10")
-
-    def crapeau(self):
-        """initialise la grille pour simuler le crapeau"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i][j - 1].naitre()
-        self.matrix[i][j - 1].basculer()
-
-        self.matrix[i + 1][j].naitre()
-        self.matrix[i + 1][j].basculer()
-
-        self.matrix[i + 1][j + 1].naitre()
-        self.matrix[i + 1][j + 1].basculer()
-
-        self.matrix[i + 1][j + 2].naitre()
-        self.matrix[i + 1][j + 2].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("10")
-
-    def escalier(self):
-        """initialise la grille pour simuler l'escalier qui se transforme en 4 blocs stables"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i + 1][j].naitre()
-        self.matrix[i + 1][j].basculer()
-
-        self.matrix[i + 1][j - 1].naitre()
-        self.matrix[i + 1][j - 1].basculer()
-
-        self.matrix[i + 2][j - 1].naitre()
-        self.matrix[i + 2][j - 1].basculer()
-
-        self.matrix[i + 2][j - 2].naitre()
-        self.matrix[i + 2][j - 2].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("63")
-
-    def pulsard(self):
-        """initialise la grille pour simuler le pulsard qui oscille avec une période de 3"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        self.matrix[i + 4][j].naitre()
-        self.matrix[i + 4][j].basculer()
-
-        for k in range(5):
-            self.matrix[i + k][j - 2].naitre()
-            self.matrix[i + k][j - 2].basculer()
-
-            self.matrix[i + k][j + 2].naitre()
-            self.matrix[i + k][j + 2].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("50")
-
-    def pentadecathlon(self):
-        """initialise la grille pour simuler le pentadecathlon qui oscille avec une période de 15"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = self.nbColonnes // 2 - 1
-
-        self.matrix[i][j].naitre()
-        self.matrix[i][j].basculer()
-
-        for k in range(1, 5):
-            self.matrix[i][j - k].naitre()
-            self.matrix[i][j - k].basculer()
-
-        for k in range(1, 6):
-            self.matrix[i][j + k].naitre()
-            self.matrix[i][j + k].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("45")
-
-    def vaisseau_spatial(self):
-        """initialise la grille pour simuler le vaisseau spatial capable de se déplacer"""
-        self.remplir_alea(0)
-        i = self.nbLignes // 2 - 1
-        j = 2
-
-        self.matrix[i + 1][j].naitre()
-        self.matrix[i + 1][j].basculer()
-
-        self.matrix[i + 3][j].naitre()
-        self.matrix[i + 3][j].basculer()
-
-        self.matrix[i][j + 1].naitre()
-        self.matrix[i][j + 1].basculer()
-
-        self.matrix[i][j + 2].naitre()
-        self.matrix[i][j + 2].basculer()
-
-        self.matrix[i + 4][j + 2].naitre()
-        self.matrix[i + 4][j + 2].basculer()
-
-        self.matrix[i][j + 3].naitre()
-        self.matrix[i][j + 3].basculer()
-
-        self.matrix[i + 4][j + 3].naitre()
-        self.matrix[i + 4][j + 3].basculer()
-
-        self.matrix[i][j + 4].naitre()
-        self.matrix[i][j + 4].basculer()
-
-        self.matrix[i][j + 5].naitre()
-        self.matrix[i][j + 5].basculer()
-
-        self.matrix[i + 3][j + 5].naitre()
-        self.matrix[i + 3][j + 5].basculer()
-
-        self.matrix[i][j + 6].naitre()
-        self.matrix[i][j + 6].basculer()
-
-        self.matrix[i + 1][j + 6].naitre()
-        self.matrix[i + 1][j + 6].basculer()
-
-        self.matrix[i + 2][j + 6].naitre()
-        self.matrix[i + 2][j + 6].basculer()
-
-        self.winfo_toplevel().currentIter.set("0")
-        self.winfo_toplevel().nIteration.set("60")
-
-    '''
-    
 if __name__ == "__main__":
 
     maGrille = GrilleFrame(master=None, nbLignes=15, nbColonnes=10, typeg='plane')
@@ -346,3 +103,23 @@ if __name__ == "__main__":
     assert len(maGrille.get_voisins(1, 1)) == 8
 
 
+
+    # répétition des tests avec une grille torique
+    maGrille2 = GrilleFrame(master=None, nbLignes=15, nbColonnes=10, typeg='torique')
+    
+    # test d'accès au attributs
+    assert maGrille2.nbLignes == 15
+    assert maGrille2.nbColonnes == 10
+    assert maGrille2.get_nbLignes() == 15
+    assert maGrille2.get_nbColonnes() == 10    
+    
+    # test de la méthode dans_grille()
+    assert maGrille2.dans_grille(0,0) == True
+    assert maGrille2.dans_grille(15,10) == False
+    assert maGrille2.dans_grille(8,8) == True
+   
+    # test de la méthode get_voisins()
+    assert len(maGrille2.get_voisins(0,0)) == 8
+    assert len(maGrille2.get_voisins(0,1)) == 8
+    assert len(maGrille2.get_voisins(1,0)) == 8
+    assert len(maGrille2.get_voisins(1,1)) == 8
